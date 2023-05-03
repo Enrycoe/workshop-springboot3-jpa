e014ef9b42088d46e29cc85ac72163cf1aa01c95
Henrique Rafael Ardina <henrique.ardina@gmail.com>
2023-05-03 20:31:00 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/ardinahenrique/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.ardinahenrique.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ardinahenrique.course.entities.Order;
+
+public interface OrderRepository  extends JpaRepository<Order, Long>{
+
+}
